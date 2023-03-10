f2887bf49cc3297857f661a2a6e3c87b46e4113d
shreydan <iamshreydan@gmail.com>
2023-03-10 22:56:30 +0530

    prettier :)

--- App.js
+++ App.js
@@ -10,30 +10,32 @@ import Navbar from "./Components/Navbar";
 const Stack = createNativeStackNavigator();
 
 const { LightTheme } = adaptNavigationTheme({
-  reactNavigationLight: DefaultTheme,
+	reactNavigationLight: DefaultTheme,
 });
 
 function App() {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  return (
-    <NavigationContainer theme={LightTheme}>
-      <Stack.Navigator
-        initialRouteName="HackerFeed"
-        screenOptions={{
-          header: (props) => <Navbar {...props} title={props.route.name} />,
-        }}
-      >
-        <Stack.Screen name="HackerFeed" component={Feed} />
-      </Stack.Navigator>
+	return (
+		<NavigationContainer theme={LightTheme}>
+			<Stack.Navigator
+				initialRouteName="HackerFeed"
+				screenOptions={{
+					header: (props) => (
+						<Navbar {...props} title={props.route.name} />
+					),
+				}}
+			>
+				<Stack.Screen name="HackerFeed" component={Feed} />
+			</Stack.Navigator>
 
-      <StatusBar style="auto" />
-    </NavigationContainer>
-  );
+			<StatusBar style="auto" />
+		</NavigationContainer>
+	);
 }
 
 export default () => (
-  <PaperProvider theme={MD3LightTheme}>
-    <App />
-  </PaperProvider>
+	<PaperProvider theme={MD3LightTheme}>
+		<App />
+	</PaperProvider>
 );
--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,27 +3,27 @@ import { useTheme } from "react-native-paper";
 import { Text } from "react-native-paper";
 
 export default function Navbar({ navigation, back, title }) {
-  const theme = useTheme();
-  return (
-    <Appbar.Header
-      mode="center-aligned"
-      style={{
-        backgroundColor: theme.colors.primaryContainer,
-      }}
-    >
-      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-      <Appbar.Content
-        title={
-          <Text
-            variant="titleLarge"
-            style={{
-              color: theme.colors.primary,
-            }}
-          >
-            {title}
-          </Text>
-        }
-      />
-    </Appbar.Header>
-  );
+	const theme = useTheme();
+	return (
+		<Appbar.Header
+			mode="center-aligned"
+			style={{
+				backgroundColor: theme.colors.primaryContainer,
+			}}
+		>
+			{back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+			<Appbar.Content
+				title={
+					<Text
+						variant="titleLarge"
+						style={{
+							color: theme.colors.primary,
+						}}
+					>
+						{title}
+					</Text>
+				}
+			/>
+		</Appbar.Header>
+	);
 }
