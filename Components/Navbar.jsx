eda0ce64afc7a7457bb9efce4f9ef4274d3bac8f
shreydan <iamshreydan@gmail.com>
2023-03-10 22:54:18 +0530

    custom navigation bar

---
+++ Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Appbar } from "react-native-paper";
+import { useTheme } from "react-native-paper";
+import { Text } from "react-native-paper";
+
+export default function Navbar({ navigation, back, title }) {
+  const theme = useTheme();
+  return (
+    <Appbar.Header
+      mode="center-aligned"
+      style={{
+        backgroundColor: theme.colors.primaryContainer,
+      }}
+    >
+      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content
+        title={
+          <Text
+            variant="titleLarge"
+            style={{
+              color: theme.colors.primary,
+            }}
+          >
+            {title}
+          </Text>
+        }
+      />
+    </Appbar.Header>
+  );
+}
