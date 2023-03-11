ed8d8e35a8a60a997b13531befa1fca2ffae14f6
shreydan <iamshreydan@gmail.com>
2023-03-11 18:56:07 +0530

    remove console.log

--- utils/urlparser.js
+++ utils/urlparser.js
@@ -1,7 +1,6 @@
 export function feedURL(rawURL) {
 	var matches = rawURL.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
 	var domain = matches && matches[1];
-	console.log(matches);
 	return {
 		url: rawURL,
 		domain: domain,
