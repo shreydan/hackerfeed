446fd20aa139f382a14548a92f8c0014c98e96b6
shreydan <iamshreydan@gmail.com>
2023-03-11 18:06:34 +0530

    post page works + layout

--- Screens/Post.jsx
+++ Screens/Post.jsx
@@ -1,7 +1,21 @@
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import {
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	View,
+	useWindowDimensions,
+} from "react-native";
 import React, { useEffect } from "react";
-import { Text, Surface, Badge, useTheme, Button } from "react-native-paper";
+import {
+	Text,
+	Surface,
+	Badge,
+	useTheme,
+	Button,
+	Divider,
+} from "react-native-paper";
 import { feedURL } from "../utils/urlparser";
+import RenderHTML from "react-native-render-html";
 
 const Post = ({ route }) => {
 	const theme = useTheme();
@@ -10,51 +24,65 @@ const Post = ({ route }) => {
 
 	return (
 		<SafeAreaView style={[styles.container]}>
-			<Surface style={[styles.surface]} elevation={0}>
-				<View style={styles.badgeContainer}>
-					<Badge
-						style={[
-							styles.badge,
-							{ backgroundColor: theme.colors.primary },
-						]}
-						size={30}
-					>
-						{item.score}
-					</Badge>
-					<Badge
-						style={[
-							styles.badge,
-							{ backgroundColor: theme.colors.secondary },
-						]}
-						size={30}
-					>
-						{item.type}
-					</Badge>
-					<Badge
-						style={[
-							styles.badge,
-							{ backgroundColor: theme.colors.inversePrimary },
-						]}
-						size={30}
-					>
-						{`${item.kids ? item.kids.length : 0} comments`}
-					</Badge>
-				</View>
-				<Text variant="titleLarge">{item.title}</Text>
-				{item.url && (
-					<Button
-						onPress={() => {
-							console.log("pressed");
-						}}
-						style={[styles.url]}
-						mode="contained"
-						icon="link"
-						accessibilityHint="click to open the webpage for the post"
-					>
-						{feedURL(item.url).domain}
-					</Button>
-				)}
-			</Surface>
+			<ScrollView style={{ width: "100%" }}>
+				<Surface style={[styles.surface]} elevation={0}>
+					<View style={styles.badgeContainer}>
+						<Badge
+							style={[
+								styles.badge,
+								{ backgroundColor: theme.colors.primary },
+							]}
+							size={30}
+						>
+							{item.score}
+						</Badge>
+						<Badge
+							style={[
+								styles.badge,
+								{ backgroundColor: theme.colors.secondary },
+							]}
+							size={30}
+						>
+							{item.type}
+						</Badge>
+						<Badge
+							style={[
+								styles.badge,
+								{
+									backgroundColor:
+										theme.colors.inversePrimary,
+								},
+							]}
+							size={30}
+						>
+							{`${item.kids ? item.kids.length : 0} comments`}
+						</Badge>
+					</View>
+					<Text variant="titleLarge">{item.title}</Text>
+					{item.url && (
+						<Button
+							onPress={() => {
+								console.log("pressed");
+							}}
+							style={[styles.url]}
+							mode="contained"
+							icon="link"
+							accessibilityHint="click to open the webpage for the post"
+						>
+							{feedURL(item.url).domain}
+						</Button>
+					)}
+					{item.text && (
+						<View style={{ marginVertical: 32 }}>
+							<RenderHTML
+								source={{ html: item.text }}
+								contentWidth={useWindowDimensions().width}
+								baseStyle={{ fontSize: 16 }}
+							/>
+						</View>
+					)}
+				</Surface>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
@@ -62,15 +90,13 @@ const Post = ({ route }) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		width: "100%",
 		alignItems: "flex-start",
 		justifyContent: "flex-start",
-		paddingHorizontal: 5,
 	},
 	surface: {
-		width: "100%",
+		flex: 1,
 		marginTop: 8,
-		paddingHorizontal: 8,
+		paddingHorizontal: 16,
 		paddingVertical: 8,
 	},
 	badgeContainer: {
@@ -84,6 +110,8 @@ const styles = StyleSheet.create({
 	},
 	url: {
 		marginTop: 16,
+		flex: 1,
+		width: "100%",
 	},
 });
 
